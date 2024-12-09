7f8a7bca2a6081e7737e22558c934c8d9d2a7b88
foldicintia <foldicintia2002@gmail.com>
2024-12-09 10:16:41 +0100

    f1 VEGE

--- src/main/java/hu/szamalk/Tabla.java
+++ src/main/java/hu/szamalk/Tabla.java
@@ -1,4 +1,30 @@
 package hu.szamalk;
 
 public class Tabla {
+
+    private char [][] T = new char [8][8] ;
+    private char UresCella;
+
+    public Tabla(char UresCella) {
+        this.T = T;
+        new char T [UresCella][UresCella];
+    }
+
+    public void Megjelenit() {
+        for (int i = 0; i < T.length; i++) {
+
+            }
+        }
+    }
+    public void Elhelyez(char N){
+
+    }
+    public boolean UresOszlop(){
+        
+    }
+    public boolean UresSor(){
+
+    }
+
+
 }
